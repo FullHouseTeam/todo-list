924f134281ad50b49f956f92bd1eb1601f08abb9
Hugo Oropeza <oropezahugo35@gmail.com>
2023-11-01 18:17:52 -0400

    feat: todolisthoropezaexample

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ListComponent } from './list/list.component';
+import { InputComponent } from './input/input.component';
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule],
+  declarations: [
+    AppComponent,
+    ListComponent,
+    InputComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
